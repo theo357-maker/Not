6096179c7e2b7190b4c764f712b03b32ce210fad
The Pigalle <theombowa2@gmail.com>
2026-02-02 15:36:22 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBn7VIddclO7KtrXb5sibCr9SjVLjOy-qI",
+  authDomain: "theo1d.firebaseapp.com",
+  projectId: "theo1d",
+  messagingSenderId: "269629842962",
+  appId: "1:269629842962:web:a80a12b04448fe1e595acb"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon-192.png"
+    }
+  );
+});
